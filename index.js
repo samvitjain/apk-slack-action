5e57baacef24de3a790b78262fd0ed8414af6c4f
Samvit <samvit@flick2know.com>
2021-01-02 09:58:13 +0530

    Fix bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios').default;
 const fs = require('fs');
-
+const FormData = require('form-data');
 // try {
 //   // `who-to-greet` input defined in action metadata file
 //   const nameToGreet = core.getInput('who-to-greet');
